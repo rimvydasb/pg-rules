a5f4331d1273525d642fceca318da8de86e11e82
Rimvydas Bingelis <rimvydas.bin@gmail.com>
2025-08-10 22:35:37 +0300

    added comments

--- src/MatchRule.ts
+++ src/MatchRule.ts
@@ -2,8 +2,24 @@
  * Represents a match rule that can be applied to database records
  */
 export interface MatchRule<T = any> {
+    /**
+     * Name of the rule, used for identification and logging.
+     * Rule name can be added to "appliedRules" field in the database to track which rules were applied.
+     */
     readonly ruleName: string;
+
+    /**
+     * Conditions to match records against.
+     * Values will be matched as regex patterns if they are strings
+     * and direct equality for other types.
+     * For example, { "category": "Groceries|Food" } will match records where category is either "Groceries" or "Food".
+     */
     readonly match: Partial<T>;
+
+    /**
+     * Changes to apply to matched records.
+     * This object will be used in the SET clause of the SQL UPDATE statement.
+     */
     readonly apply: Partial<T>;
 }
 
